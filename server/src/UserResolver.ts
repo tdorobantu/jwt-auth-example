dd1bfd23bdf6f89530eb8cbbbbc996d12052a001
tdorobantu <tdorobantu95@gmail.com>
2021-09-02 15:40:12 +0300

    Working on bye function // @1.07.17 👋

---
+++ server/src/UserResolver.ts
@@ -0,0 +1,88 @@
+import {
+  Arg,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  Field,
+  Ctx,
+  UseMiddleware,
+} from "type-graphql";
+import { hash, compare } from "bcryptjs";
+import { User } from "./entity/User";
+import { MyContext } from "./MyContext";
+import { createAccessToken, createRefreshToken } from "./auth";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "hi!";
+  }
+
+  @Query(() => String)
+  @UseMiddleware()
+  bye(@Ctx() { payload }: MyContext) {
+    console.log(payload);
+    return `your user id is: ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Could not find user!");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Wrong password");
+    }
+
+    //login succesful
+
+    res.cookie("jid", createRefreshToken(user), {
+      httpOnly: true,
+    });
+
+    return {
+      accessToken: createAccessToken(user),
+    };
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ server/src/isAuth.ts
@@ -0,0 +1,22 @@
+import { verify } from "jsonwebtoken";
+import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "./MyContext";
+
+// bearer 14r2rf2ff334rf4
+
+export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
+  const authorization = context.req.headers["authorization"];
+
+  if (!authorization) {
+    throw new Error("Not authenticated.");
+  }
+
+  try {
+    const token = authorization?.split(" ")[1];
+    const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+    context.payload = payload as any;
+  } catch (err) {
+    console.log(err);
+  }
+  return next();
+};
