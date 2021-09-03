a7de9645e8d78cb6d45bf801992778d937b3cb08
tdorobantu <tdorobantu95@gmail.com>
2021-09-03 16:06:09 +0300

    graphql-codegen setup done 🙌

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,15 @@
-import { useQuery } from '@apollo/react-hooks';
+import {useHelloQuery} from './generated/graphql'
 import React from 'react';
 import {gql} from "apollo-boost"
 
 function App() {
 
-  const {data, loading} = useQuery(gql`
-  {
-    hello
-  }
-  `)
-  
-if(loading) { return (
+  const {data, loading} = useHelloQuery()
+
+if(loading || !data) { return (
   <div className="">loading...</div>
 );}
- return <div>{JSON.stringify(data)}</div>
+ return <div>{data.hello}</div>
 }
 
 export default App;
